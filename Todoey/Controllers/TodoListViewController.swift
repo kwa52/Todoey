51b920cceadd95b3352b1445955cc97c1dc2a6f5
kwa52 <kwa52@sfu.ca>
2018-02-24 08:47:40 -0800

    Persist data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -14,13 +14,18 @@ class TodoListViewController: UITableViewController {
     
     let defaults = UserDefaults.standard
 
+    // get file path of where the plist resides and append a new plist
+    let dataFilePath = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = UserDefaults.standard.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items
-        }
+//        if let items = UserDefaults.standard.array(forKey: "TodoListArray") as? [Item] {
+//            itemArray = items
+//        }
+        
+        loadItems()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,14 +33,15 @@ class TodoListViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //
     //MARK: - TableView Datasource Methods
+    //
     
     // create tableView cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        // reuse a cell object for return
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        
         let item = itemArray[indexPath.row]
         
         cell.textLabel?.text = item.title
@@ -46,28 +52,34 @@ class TodoListViewController: UITableViewController {
         else {
             cell.accessoryType = .none
         }
-        
         return cell
     }
     
+    // tell data source how many cells to display
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
+    //
     //MARK: - TableView Delegate Methods
+    //
     
+    // Action when select on a cell
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.reloadData()
+        saveItems()
         
         // remove the grayed out background when tapping on a cell
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //
     //MARK: - Add New Item
+    //
     
+    // Action when add button is pressed
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
   
         var textField = UITextField()
@@ -81,9 +93,10 @@ class TodoListViewController: UITableViewController {
             
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            // store custom objects using UserDefault will crash the app
+            //self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            self.tableView.reloadData()
+            self.saveItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -94,6 +107,40 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    //
+    //MARK: - Model Manipulation methods
+    //
+    
+    // Update custom plist file
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }
+        catch {
+            print("Error encoding array, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            
+            let decoder = PropertyListDecoder()
+            
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch {
+                print(error)
+            }
+            
+        }
     }
 }
 
